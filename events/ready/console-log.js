80464db187caf88bfe6412d8be55304c736c7450
akshtt-dev <akshtt.dev@gmail.com>
2025-06-17 14:47:57 +0530

    fix: correct typo in Pterodactyl panel URL in console log event

--- events/ready/console-log.js
+++ events/ready/console-log.js
@@ -2,7 +2,7 @@ import { ActivityType } from "discord.js";
 
 export default (client) => {
   console.log(`Logged in as ${client.user.tag}`);
-  client.user.setActivity(`${client.config.PTERODACTLY_PANEL.panelURL}`, {
+  client.user.setActivity(`${client.config.PTERODACTYL_PANEL.panelURL}`, {
     type: ActivityType.Watching,
   });
 };
